fba9109bd0b55439f060a07b92f43714325e30fc
Daniel Rodrigues <danielwalterrodrigues@gmail.com>
2023-12-27 19:56:20 -0300

    solving cors

--- pages/api/pt/index.ts
+++ pages/api/pt/index.ts
@@ -3,11 +3,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
 
     try {
-        res.status(200).json([
+        res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.status(200).json(
             { id: 1, name: 'Daniel'}
-        ])
+        )
     } catch (err) {
-        res.status(500).json({ statusCode: 500, message: err.message})
+        res.status(500).json({ statusCode: 500, message: err.message});
+        
     }
 }
 
