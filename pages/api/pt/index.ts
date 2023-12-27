28aa9358eca49af5d042318a844d284a0216d28a
Daniel Rodrigues <danielwalterrodrigues@gmail.com>
2023-12-26 23:16:23 -0300

    testando conexao

--- pages/api/pt/index.ts
+++ pages/api/pt/index.ts
@@ -4,9 +4,7 @@ const handler = (_req: NextApiRequest, res: NextApiResponse) => {
 
     try {
         res.status(200).json([
-            { id: 1, name: 'Daniel'},
-            { id: 2, name: 'Walter'},
-            { id: 3, name: 'Rodrigues'}
+            { id: 1, name: 'Daniel'}
         ])
     } catch (err) {
         res.status(500).json({ statusCode: 500, message: err.message})
